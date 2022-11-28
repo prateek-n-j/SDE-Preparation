fb90d8ae20df5413112338da72734434e89ef7ec
Prateek N J <prateekpnj@gmail.com>
2022-11-28 11:51:26 +0530

    Create Cpp basics.cpp

---
+++ Cpp basics.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //int
+    int a=2, b=3;
+    int sum = a+b;
+    cout<<sum<<endl;
+    //char
+    char c = 'b';
+    cout<<c<<endl;
+    //float
+    float d = 3.141678;
+    cout<<d<<endl;
+    //double
+    double e = 3.141781369784;
+    cout<<e<<endl;
+    //long int and long long int
+    return 0;
+}
